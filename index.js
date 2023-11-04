32bcc3cd68cbd509e859e8548842161d728f72a0
hayes28 <heather.hayes918@gmail.com>
2023-11-03 21:00:46 -0500

    Add single queries for game, author, and review. Add nested queries for a game's reviews, an author's reviews, and a review's author and game.

--- index.js
+++ index.js
@@ -13,20 +13,44 @@ const resolvers = {
         games() {
             return db.games
         },
+        // query for a single game
         game(_, args) {
             return db.games.find(game => game.id === args.id)
         },
         authors() {
             return db.authors
         },
+        // query for a single author
         author(_, args) {
             return db.authors.find(author => author.id === args.id)
         },
         reviews() {
             return db.reviews
         },
+        // query for a single review
         review(_, args) {
             return db.reviews.find(review => review.id === args.id)
+        }
+    },
+    // query for a game's reviews
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.game_id === parent.id)
+        }
+    },
+    // query for an author's reviews
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((r) => r.author_id === parent.id)
+        }
+    },
+    // query for a review's author and game
+    Review: {
+        game(parent) {
+            return db.games.find((g) => g.id === parent.game_id)
+        },
+        author(parent) {
+            return db.authors.find((a) => a.id === parent.author_id)
         }
     }
 }
