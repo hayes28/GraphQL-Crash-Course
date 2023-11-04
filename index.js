0618389f614c465411aeb01e44d6f7b07c1bca8d
hayes28 <heather.hayes918@gmail.com>
2023-11-03 19:45:28 -0500

    Update Apollo Server setup and add resolvers

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-import { ApolloServer } from 'apollo-server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from '@apollo/server'
+import { startStandaloneServer } from '@apollo/server/standalone'
+
+// Import data
+import db from './_db.js';
 
 // Import typeDefs
 import { typeDefs } from './schema.js';
 
+// resolvers
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        authors() {
+            return db.authors
+        },
+        reviews() {
+            return db.reviews
+        },
+    }
+}
+
 // Setup Apollo Server
 const server = new ApolloServer({
     typeDefs,
-    // resolvers
+    resolvers
 });
 
 // Start the server
@@ -15,4 +33,4 @@ const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 }
 })
 
-console.log(`Server ready at port`, 4000);
+console.log(`Server ready at: ${url}`);
